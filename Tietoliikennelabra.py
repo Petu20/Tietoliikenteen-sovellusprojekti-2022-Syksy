3c1c78ecd6fe466b6fb151eefe73133f2bf03f99
Aki-Petteri Leppänen <94763177+Petu20@users.noreply.github.com>
2022-12-13 01:07:51 +0200

    Update Tietoliikennelabra.py

--- Tietoliikennelabra.py
+++ Tietoliikennelabra.py
@@ -1,15 +1,18 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("172.20.241.9", 20000))
-s.sendall(b"toimiiko")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(("172.20.241.9", 20000))
+    s.sendall(b"56\n")
+    data = s.recv(1024)
 
-data = s.recv(1024)
-print("Received {data!r}") [699:] #palauttaa viimeiset 300 riviä
+list = []
+list.append(data.decode("utf-8"))
+
+for i in list:
+    print(i, end = "")
 
 s.close
 
-    
-  
+#toimi myös teemun esimerkillä. sillä kyllä sai enemmän dataa ulos kuin tuon 1 rivin
 
         
