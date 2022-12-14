9dc01be271e4611f6e2c7fbd7238f1a9e26d8138
Aki-Petteri Leppänen <94763177+Petu20@users.noreply.github.com>
2022-12-14 18:16:14 +0200

    Update hakeminen.py

--- hakeminen.py
+++ hakeminen.py
@@ -2,11 +2,8 @@ import requests as re
 import pandas as pd
 
 r = re.get("http://172.20.241.9/luedataa_kannasta_groupid_csv.php?groupid=74")
-print(r.text) [:200] #printtaa ensimmäiset 200 sanaa ylemmästä sivusta
+print(r.text)
 
-kantatieto = pd.DataFrame(r.text)
-kantatieto.to_csv
-
-#http://172.20.241.9/luedataa_kannasta_groupid_csv.php?groupid=XXX
-
-#"https://tl.oamk.fi/tietoliikenteen_sovellusprojekti/" tää toimii
+url = "http://172.20.241.9/luedataa_kannasta_groupid_csv.php?groupid=74"
+csv = pd.read_csv(url, delimiter=';')
+csv.to_csv('groupid74data.csv')
